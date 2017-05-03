78877d15ef1c8e02c6c11243b1a15390ed637ca9
Darwin Huang <huangdarwin2@gmail.com>
2017-05-03 17:58:52 -0400

    nerf yellow, random initial angle for black characters

--- js/gfx/camera.js
+++ js/gfx/camera.js
@@ -6,57 +6,12 @@ function Camera(cvs){
     var at  = vec3(0.0, 0.0, 0.0);     //camera faces this location
     const up  = vec3(0.0, 1.0, 0.0);     //orientation of camera, where up is above the camera
 
-    // var trackingMouse = false;
-    // var oldX, oldY;
-    // var theta = [0,0,0];
-
-    // //add event listeners
-    // cvs.addEventListener("mousedown", beginTrackingMouse, false);
-    // cvs.addEventListener("mouseup", stopTrackingMouse, false);
-    // cvs.addEventListener("mouseout", stopTrackingMouse, false);
-    // cvs.addEventListener("mousemove", mouseMove, false);
-
-    // function beginTrackingMouse(e){
-    //     trackingMouse = true;
-    //     oldX = e.pageX;
-    //     oldY = e.pageY;
-    //     e.preventDefault(); //Prevents being highlighted when double-clicked on desktop, scrolling on mobile
-    // }
-
-    // function stopTrackingMouse(e){
-    //     trackingMouse = false;
-    // }
-
-    // function mouseMove(e){
-    //     if(!trackingMouse){return;}
-
-    //     var dx = e.pageX - oldX;
-    //     var dy = e.pageY - oldY;
-
-    //     setCameraAngle(180 * dx/canvas.width, 180 * dy/canvas.height);
-    //     oldX = e.pageX;
-    //     oldY = e.pageY;
-    // }
-
-    // function setCameraAngle(dx, dy){
-    //     var dir = [-1, -1];
-    //     if(theta[0] > 90 && theta[0] < 270){
-    //         dir[1] = 1;
-    //     }
-
-    //     theta[0] = (theta[0] - (dir[0] * dy)) % 360;    //rotate about x axis to have y move
-    //     theta[1] = (theta[1] - (dir[1] * dx)) % 360;    //rotate about y axis to have x move
-    //     console.log(theta);
-    // }
-
     this.setCenter = function(x,y){
         eye = vec3(x,y,eyeDistance);
         at  = vec3(x,y,0);
     }
 
     this.getViewMatrix = function(){
-        // var rotationMatrix = mult(rotateX(theta[0]), rotateY(theta[1]));    //to move camera to view of camera
-        // return mult(lookAt(eye, at, up), rotationMatrix);
         return lookAt(eye,at,up);
     }   
 }
--- js/npc.js
+++ js/npc.js
@@ -1,6 +1,6 @@
 //immobile, sprays bullets in all directions
 function NPC1(origX, origY){
-	id = -1
+	id = -1;
 	Character.call(this, origX, origY, id, color.brown);
 	this.turret = new Turret({bulletSpeed: .2, rotationSpeed: 10, delay: 80, baseOffset: 1.7, diameter: .8});
 	this.base = new Ball(origX, origY, 1.7, this.material);
@@ -18,9 +18,9 @@ NPC1.prototype.input = function(options){
 
 //slow, aims at player w/single bullets
 function NPC2(origX, origY){
-	id = -1
+	id = -1;
 	Character.call(this, origX, origY, id, color.black);
-	this.turret = new Turret({bulletSpeed: .8, rotationSpeed: 1, delay: 250, baseOffset: 1.5, diameter: .7});
+	this.turret = new Turret({bulletSpeed: .8, rotationSpeed: 1, delay: 250, baseOffset: 1.5, diameter: .7, angle: getRandomInt(0,360)});
 	this.base = new Ball(origX, origY, 1.5, this.material);
 	this.moveSpeed = .06;
 }
@@ -50,7 +50,7 @@ NPC2.prototype.input = function(options){
 
 //three-bullet spread, medium speed
 function NPC3(origX, origY){
-	id = -3
+	id = -3;
 	Character.call(this, origX, origY, id, color.green);
 	this.turret = new Turret({bulletSpeed: .4, rotationSpeed: 20, delay: 10, baseOffset: 1.5, diameter: .7});
 	this.base = new Ball(origX, origY, 1.5, this.material);
@@ -71,11 +71,11 @@ NPC3.prototype.input = function(options){
 
 //runs towards player to ram them, fast
 function NPC4(origX, origY){
-	id = -4
+	id = -4;
 	Character.call(this, origX, origY, id, color.yellow);
 	this.turret = new Turret({bulletSpeed: .25, rotationSpeed: 20, delay: 10, baseOffset: 1.1, diameter: .7});
 	this.base = new Ball(origX, origY, 1.1, this.material);
-	this.moveSpeed = .2;
+	this.moveSpeed = .18;
 }
 
 NPC4.prototype = Object.create(Character.prototype);
